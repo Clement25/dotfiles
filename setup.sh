07bcf569bfaa480e7c3c29d9176e6d6cb4f9505b
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-08-14 19:09:27 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,22 @@ ln -s Github/dotfiles/.tmux.conf .
 ln -s Github/dotfiles/.toprc .
 ln -s Github/dotfiles/.vimrc .
 ln -s Github/dotfiles/.ssh .
+
+# install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
+
+# jupyter notebook without password
+conda install jupyter
+python jupyter_without_password_setup.py
+
+# vim bundle manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#
 source ~/.bashrc
-#sudo apt-get install tmux
-#sudo apt-get install ctags
+
+# install software for efficiency
+sudo apt-get install tmux
+sudo apt-get install ctags
