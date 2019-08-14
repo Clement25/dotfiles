055dbb258fa57b05dd29c6bc16740b7a02a13c41
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-08-14 19:04:58 +0800

    successfully generate jupyter notebook without password

--- jupyter_notebook_config.py
+++
@@ -1,7 +0,0 @@
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = 'sha1:somethings-here'
-
-c.NotebookApp.port = 8889
-# c.NotebookApp.notebook_dir = '/which/path/to/save/file'
-
--- jupyter_without_password_setup.py
+++ jupyter_without_password_setup.py
@@ -1,3 +1,24 @@
 from notebook.auth import passwd
+import os
+## the python file came from this tutorial
+## https://www.jianshu.com/p/08f276d48669?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
+generated_password = passwd()
+string = "c.NotebookApp.ip = '*'\n" + "c.NotebookApp.open_browser = False\n" + \
+        "c.NotebookApp.password = '{passwd}'\n".format(passwd=generated_password) + \
+        "c.NotebookApp.port = 8889\n" + \
+        "# c.NotebookApp.notebook_dir = '/which/path/to/save/file'"
 
-passwd()
+with open('jupyter_notebook_config.py', 'w') as f:
+    f.write(string)
+
+home_dir = os.path.expanduser('~')
+jupyter_path = os.path.join(home_dir, '.jupyter')
+
+if not os.path.exists(jupyter_path): 
+    print("Making folder '.jupyter' in home directory")
+    os.mkdir(jupyter_path)
+
+cmd = 'mv jupyter_notebook_config.py ' + os.path.join(jupyter_path, 'jupyter_notebook_config.py') 
+print(cmd)
+os.system(cmd)
+
