3f72bc66db5523ed1e383d102bd5de9b97bb83c8
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-11-29 21:26:15 +0700

    Add installing extension in jupyter notebook setup

--- utils/setup-remote-jupyter.sh
+++ utils/setup-remote-jupyter.sh
@@ -8,3 +8,12 @@ else
     echo "Skipped setting up jupyter without password"
 fi
 
+echo -e "Make sure you have activated the conda environment you wish to install jupyter"
+read -p "Install jupyter notebook extenstions? y or n?" ext
+if [ "$ext" == "y" ]; then
+    echo "Installing jupyter notebook extensions"
+    conda install -c conda-forge jupyter_contrib_nbextensions
+else
+    echo "Skipped installing jupyter notebook extensions"
+
+echo -e "Success"
